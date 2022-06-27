4cfc4629205d0aab7e43209d24f25bf1f215ab7e
Alex Yang <alex.yang1115@gmail>
2022-06-27 16:42:15 +0100

    style header

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -2,6 +2,8 @@
 	let src = 'images/owl-logo.png';
 </script>
 
-<img {src} alt="owly" />
+<header class="flex p-4 items-center">
+	<img class="w-3/12" {src} alt="owly" />
 
-<h1>Read Me A Story</h1>
+	<h1 class="text-amber-700 font-Amatic text-6xl">Read Me A Story</h1>
+</header>
