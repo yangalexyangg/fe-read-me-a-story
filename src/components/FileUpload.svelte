2447a7c27a84082e86afc1904ebc60fe79d661de
akflds <andrewkirsfelds@gmail.com>
2022-06-28 10:41:11 +0100

    create initial fileupload component with hardcoded data

--- src/components/FileUpload.svelte
+++ src/components/FileUpload.svelte
@@ -1,6 +1,40 @@
 <script lang="ts">
+	import { ref, uploadBytes, listAll } from 'firebase/storage';
+	import { storage } from '../utils/admin';
+	import file from '../data/testFile.txt';
+
+	// specify where to store recordings in firebase
+	// Date.now() is temp workaround to create unique(ish) filenames and prevent overwriting
+	const recordingRef = ref(storage, `recordings/testFile-${Date.now()}.txt`);
+
+	// TODO: convert to interface?
+	const metadata = {
+		// using customMetadata as a temporary way to store human-readable name from form
+		customMetadata: { niceName: '' }
+	};
+
+	const uploadFile = async () => {
+		console.log('uploading file');
+		const response = await fetch(file);
+		if (response.ok) {
+			const fileContents = await response.blob();
+			const snapshot = await uploadBytes(recordingRef, fileContents, metadata);
+			console.log(`${snapshot.metadata.customMetadata.niceName} uploaded!`);
+		} else {
+			// placeholder error handling
+			const message = `An error has occured: ${response.status}`;
+			throw new Error(message);
+		}
+	};
 </script>
 
-<section />
+<section>
+	<form>
+		<label
+			>File name <input type="text" required bind:value={metadata.customMetadata.niceName} /></label
+		>
+	</form>
+	<button on:click={uploadFile}>Upload</button>
+</section>
 
 <style></style>
