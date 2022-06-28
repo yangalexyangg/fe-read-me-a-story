ccd9834a5b6bd69373ed9af00428c6eaa7f6fa3c
akflds <andrewkirsfelds@gmail.com>
2022-06-28 09:12:47 +0100

    expose auth, db and storage components

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -1,3 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './config';
-export const app = initializeApp(firebaseConfig);
+
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
