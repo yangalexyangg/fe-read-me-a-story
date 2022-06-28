9ca592b59c16b14595d0967d57bedfc694ce8da3
akflds <andrewkirsfelds@gmail.com>
2022-06-28 14:48:43 +0100

    retrieve and render list of recordings

---
+++ src/components/Books.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { storage } from '../utils/admin';
+	import { ref, listAll } from 'firebase/storage';
+
+	let items = [];
+
+	const listRef = ref(storage, 'recordings');
+
+	const getBooks = async () => {
+		const res = await listAll(listRef);
+		items = [...res.items];
+	};
+
+	onMount(() => getBooks());
+</script>
+
+<ul>
+	{#each items as item}
+		<li>{item.name}</li>
+	{/each}
+</ul>
+
+<style></style>
