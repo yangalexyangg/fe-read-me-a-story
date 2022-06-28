f56d3bd1c4146872c72ac4a3454f91c17f73cbb8
akflds <andrewkirsfelds@gmail.com>
2022-06-28 10:11:56 +0100

    temporary prop to check component

--- src/routes/record.svelte
+++ src/routes/record.svelte
@@ -1,8 +1,9 @@
 <script>
 	import FileUpload from 'src/components/FileUpload.svelte';
 	import Header from '../components/Header.svelte';
+	let fileName = 'testFile.txt';
 </script>
 
 <Header />
 <h2>Record A Story</h2>
-<FileUpload />
+<FileUpload {fileName} />
