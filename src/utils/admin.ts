76863d7859ce38d74d0711523287d6461a6afe9f
akflds <andrewkirsfelds@gmail.com>
2022-06-28 09:14:04 +0100

    remove db component

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -2,11 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './config';
 
 import { getStorage } from 'firebase/storage';
-import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getDatabase(app);
 export const storage = getStorage(app);
