7a3728b4e430add3e7f16f2b517626145f0eaa8e
Alex Yang <alex.yang1115@gmail>
2022-06-28 15:43:44 +0100

    removed getNiceName

--- src/components/Books.svelte
+++ src/components/Books.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { storage } from '../utils/admin';
-	import { ref, listAll, getMetadata } from 'firebase/storage';
+	import { ref, listAll } from 'firebase/storage';
 
-	let books = [];
+	interface Book {
+		name: string
+	}
+
+	let books:Book[] = [];
 
 	const listRef = ref(storage, 'recordings');
-
-	const getNiceName = (fullPath) => {
-		const bookRef = ref(storage, fullPath);
-		return getMetadata(bookRef)
-			.then((metadata) => {
-				if (metadata.customMetadata) {
-					return metadata.customMetadata.niceName;
-				} else {
-					return '';
-				}
-			})
-			.catch((err) => console.log(err));
-	};
 
 	const getBooks = async () => {
 		const { items } = await listAll(listRef);
 		books = items.map((item) => {
-			getNiceName(item.fullPath).then((niceName) => {
-				return { name: item.name, niceName };
-			});
+			return {name: item.name}
 		});
 	};
 
-	onMount(() => getBooks());
+	onMount(() => {getBooks()});
 </script>
 
 <ul>
 	{#each books as book}
-		<li>{book.name} - {book.niceName}</li>
+		<li>{book.name}</li>
 	{/each}
 </ul>
 
