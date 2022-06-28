1fcce1f7d361ad22ca29fbe078d28a5fa609c113
akflds <andrewkirsfelds@gmail.com>
2022-06-28 14:34:32 +0100

    setup Books component

--- src/routes/bookshelf.svelte
+++ src/routes/bookshelf.svelte
@@ -1,1 +1,9 @@
-<p>bookshelf</p>
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+	import Books from '../components/Books.svelte';
+</script>
+
+<Header />
+<Books />
+
+<style></style>
