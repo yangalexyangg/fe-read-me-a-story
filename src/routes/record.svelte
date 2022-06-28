5844e9fcadf06bed0c21dbc3e22405adb9b80d52
321jellyfish <csljeffreys@gmail.com>
2022-06-28 10:43:58 +0100

    style h2

--- src/routes/record.svelte
+++ src/routes/record.svelte
@@ -4,5 +4,5 @@ import Header from '../components/Header.svelte';
 </script>
 
 <Header />
-<h2>Record A Story</h2>
+<h2 class="font-Josefin text-amber-100 text-4xl font-normal text-center">Record A Story</h2>
 <Recording/>
