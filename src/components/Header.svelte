2c5d77da1e9eb83f4a66302f79cf0fec25088bc3
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-28 11:24:13 +0100

    Added an anchor tag to the header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,13 @@
 <script>
-	let src = 'images/owl-logo.png';
+let src = 'images/owl-logo.png';
 </script>
 
-<header class="flex p-4 items-center">
-	<img class="w-3/12" {src} alt="owly" />
+<header >
+    <a href="/">
+        <section class="flex p-4 items-center">
+            <img class="w-3/12" {src} alt="owly" />
 
-	<h1 class= "font-Amatic text-6xl text-amber-100">Read Me A Story</h1>
+            <h1 class= "font-Amatic text-6xl text-amber-100">Read Me A Story</h1>
+		</section>
+    </a>
 </header>
