a44fec8d64fffceaa35168a5688dda1775bfad72
akflds <andrewkirsfelds@gmail.com>
2022-06-28 10:11:01 +0100

    add FileUpload component

--- src/routes/record.svelte
+++ src/routes/record.svelte
@@ -1,6 +1,8 @@
 <script>
+	import FileUpload from 'src/components/FileUpload.svelte';
 	import Header from '../components/Header.svelte';
 </script>
 
 <Header />
 <h2>Record A Story</h2>
+<FileUpload />
