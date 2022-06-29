5f5bf95698ce095b87a22f757a9be38463d2d035
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-29 13:04:33 +0100

    Fixed a bug where recording without uploading and then returning to the home screen wouldnt re enable the buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,16 @@
 <script>
 	import { page } from '$app/stores';
+	import {recordingIsDisabled, stopIsDisabled, filePresentToUpload} from "../store"
+const handleClick = ()=>{
+	recordingIsDisabled.set(false)
+	stopIsDisabled.set(false)
+	filePresentToUpload.set(false)
+}
 </script>
 
-<a class:active={$page.url.pathname === '/record'} sveltekit:prefetch href="/record"
+
+
+<a on:click={handleClick} class:active={$page.url.pathname === '/record'} sveltekit:prefetch href="/record"
 	>I want to record a story</a
 >
 
