8786b92dc5f3d053564b03ff7dacb496eacf07a9
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-29 11:36:26 +0100

    Added filePresentToUpload to the svelte store

--- src/store.js
+++ src/store.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 export const recordingIsDisabled = writable(false);
 export const stopIsDisabled = writable(false);
+export const filePresentToUpload = writable(false);
