cf8788e7c40484215a533cd98395ee7d995d4ed6
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-29 14:48:11 +0100

    removed the button resets from this page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,18 +3,18 @@
 	import { page } from '$app/stores';
 	import {recordingIsDisabled, stopIsDisabled, filePresentToUpload} from "../store"
 
-	const handleClick = ()=>{
-	recordingIsDisabled.set(false)
-	stopIsDisabled.set(false)
-	filePresentToUpload.set(false)
-}
+// 	const handleClick = ()=>{
+// 	recordingIsDisabled.set(false)
+// 	stopIsDisabled.set(false)
+// 	filePresentToUpload.set(false)
+// }on:click={handleClick}
 </script>
 
 
 <Header/>
 <ul class="text-center">
 	<li class="mb-11">
-		<a on:click={handleClick} class:active={$page.url.pathname === '/record'} sveltekit:prefetch href="/record" class="underline underline-offset-4 decoration-solid decoration-2 decoration-amber-100"
+		<a  class:active={$page.url.pathname === '/record'} sveltekit:prefetch href="/record" class="underline underline-offset-4 decoration-solid decoration-2 decoration-amber-100"
 	><h2 class="font-Josefin text-amber-100 text-4xl font-normal text-center">I want to record a story</h2></a>
 	</li>
 	<li>
