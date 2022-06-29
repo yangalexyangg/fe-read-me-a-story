91a6fc41fb66cbe2f6304269951f9c591b378b4d
321jellyfish <csljeffreys@gmail.com>
2022-06-29 16:30:52 +0100

    hard coded audio to individual book component

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -7,6 +7,6 @@
 <h2 class="font-Josefin text-amber-100 text-4xl font-normal text-center">{bookTitle}</h2>
 <img {src} alt={bookTitle} class=" m-auto mt-4 mb-4 max-w-[13rem]">
 <audio controls class="m-auto mt-10">
-<!-- need to add source, no audio at moment  -->
-
+<!-- need to add source dynamically  -->
+<source src="https://firebasestorage.googleapis.com/v0/b/read-me-a-story-18a8d.appspot.com/o/recordings%2Frecording-1656426424469.ogg?alt=media&token=67acd67e-8314-4ea9-b91c-872fe76f6741"/>
 </audio>
