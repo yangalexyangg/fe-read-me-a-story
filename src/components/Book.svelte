34e360ea9dfd2f0b90ff9c3b9400d3c791f2d9fc
321jellyfish <csljeffreys@gmail.com>
2022-06-29 15:05:11 +0100

    set up props

---
+++ src/components/Book.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+
+</script>
+
+<h3>Hello Book</h3>
--- src/routes/[bookTitle].svelte
+++ src/routes/[bookTitle].svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
+	import Book from '../components/Book.svelte'
 	import { page } from '$app/stores';
-
+    let bookTitle = $page.params.bookTitle
 </script>
 
 <Header />
 
 <h2 class="font-Josefin text-amber-100 text-4xl font-normal text-center">{$page.params.bookTitle}</h2>
-
+<Book bookTitle = {bookTitle} />
 <style></style>
