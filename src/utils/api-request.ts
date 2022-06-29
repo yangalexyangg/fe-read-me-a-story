35a5e810a850db1269a22b7a7dce8f22c6025958
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-29 16:53:35 +0100

    created a util func that fetchs the stories from the database

---
+++ src/utils/api-request.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const apiCall = axios.create({ baseURL: 'http://127.0.0.1:5000' });
+
+export const fetchStories = () => {
+	return apiCall.get('/stories').then((res) => {
+		return res.data;
+	});
+};
