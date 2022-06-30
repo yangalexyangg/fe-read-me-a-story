7dd3f68416457a91762de11a944e8fb5806ba380
321jellyfish <csljeffreys@gmail.com>
2022-06-30 15:00:39 +0100

    create log out page

---
+++ src/routes/logout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+	let src: string = 'images/owl-logo.png';
+</script>
+
+<img class="w-40 m-auto mt-64" {src} alt="owly" />
+<h1 class="text-amber-100 text-center">Goodnight!</h1>
+
+<a
+	class:active={$page.url.pathname === '/'}
+	sveltekit:prefetch
+	href="/"
+	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
+	><p class="text-center font-Josefin font-normal text-amber-100">
+		Sign back in?
+	</p></a
+>
