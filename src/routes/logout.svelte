fae453a7d4a95e0f045c9c9e7b36ce626f8465fa
321jellyfish <csljeffreys@gmail.com>
2022-06-30 15:06:09 +0100

    add log in button to nav bar component

--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
 	let src: string = 'images/owl-logo.png';
 </script>
 
-<img class="w-40 m-auto mt-64" {src} alt="owly" />
-<h1 class="text-amber-100 text-center">Goodnight!</h1>
+<img class="m-auto mt-64 w-40" {src} alt="owly" />
+<h1 class="text-center text-amber-100">Goodnight!</h1>
 
 <a
 	class:active={$page.url.pathname === '/'}
 	sveltekit:prefetch
 	href="/"
 	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
-	><p class="text-center font-Josefin font-normal text-amber-100">
-		Sign back in?
-	</p></a
+	><p class="text-center font-Josefin font-normal text-amber-100">Log back in?</p></a
 >
