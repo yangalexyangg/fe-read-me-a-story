dd8e7b61398c31a5dcfd02365c2f6d2066d5934c
Alex Yang <alex.yang1115@gmail>
2022-07-04 16:37:26 +0100

    add title to headbar

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -21,7 +21,9 @@
 		}
 	});
 </script>
-
+<svelte:head>
+	<title>{book.title}</title>
+</svelte:head>
 <h2 class="text-center font-Josefin text-4xl font-normal text-amber-100">{book.title}</h2>
 <img {src} alt={book.title} class=" m-auto mt-4 mb-4 max-w-[13rem]" />
 <audio controls class="m-auto mt-10" src={chapterSource} />
