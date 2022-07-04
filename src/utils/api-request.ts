f566a7d1e2717ec56c3a53c53950bf976cb20582
akflds <andrewkirsfelds@gmail.com>
2022-07-04 13:05:18 +0100

    formatting

--- src/utils/api-request.ts
+++ src/utils/api-request.ts
@@ -6,7 +6,6 @@ import axios from 'axios';
 const apiCall = axios.create({ baseURL: 'http://127.0.0.1:5000' });
 
 export const fetchStories = (familyId: string) => {
-	
 	return apiCall.get(`/stories/${familyId}`).then((res) => {
 		return res.data;
 	});
