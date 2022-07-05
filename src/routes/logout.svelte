96bf12c7cd3ee80a7415c7201ec928ef696b0317
321jellyfish <csljeffreys@gmail.com>
2022-07-05 10:36:09 +0100

    style log out screen

--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -17,8 +17,16 @@
 	});
 </script>
 
-<img class="m-auto mt-64 w-40" {src} alt="owly" />
-<h1 class="text-center text-amber-100">Goodnight!</h1>
+
+<div class="text-center m-auto text-[#b9f6ca] mt-40 animate-pulse">
+	<p class="inline text-2xl ml-5">z</p>
+	<p class="inline text-4xl">z</p>
+	<p class="inline text-6xl">z</p>
+</div>
+
+
+<img class="m-auto w-40" {src} alt="owly" />
+<h1 class="text-center text-amber-100 text-4xl my-5">Goodnight!</h1>
 
 <a
 	class:active={$page.url.pathname === '/'}
@@ -27,3 +35,5 @@
 	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
 	><p class="text-center font-Josefin font-normal text-amber-100">Log back in?</p></a
 >
+
+
