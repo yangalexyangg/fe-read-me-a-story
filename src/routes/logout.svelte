163272ffc46830c98a8602ddbf362de0f042bd86
321jellyfish <csljeffreys@gmail.com>
2022-07-05 10:37:43 +0100

    reformatting

--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -17,16 +17,14 @@
 	});
 </script>
 
-
-<div class="text-center m-auto text-[#b9f6ca] mt-40 animate-pulse">
-	<p class="inline text-2xl ml-5">z</p>
+<div class="m-auto mt-40 animate-pulse text-center text-[#b9f6ca]">
+	<p class="ml-5 inline text-2xl">z</p>
 	<p class="inline text-4xl">z</p>
 	<p class="inline text-6xl">z</p>
 </div>
 
-
 <img class="m-auto w-40" {src} alt="owly" />
-<h1 class="text-center text-amber-100 text-4xl my-5">Goodnight!</h1>
+<h1 class="my-5 text-center text-4xl text-amber-100">Goodnight!</h1>
 
 <a
 	class:active={$page.url.pathname === '/'}
@@ -35,5 +33,3 @@
 	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
 	><p class="text-center font-Josefin font-normal text-amber-100">Log back in?</p></a
 >
-
-
