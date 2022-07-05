7415b2d8ebaf44ecb28a9ec4320f359f0f114377
321jellyfish <csljeffreys@gmail.com>
2022-07-05 10:21:07 +0100

    reformat

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,34 +2,20 @@
 	import { page } from '$app/stores';
 </script>
 
-<nav class="flex gap-4 justify-center py-3 mx-3 mb-6 border-y-2 border-[#b9f6ca]">
-	<a
-		class:active={$page.url.pathname === '/record'}
-		sveltekit:prefetch
-		href="/record"
+<nav class="mx-3 mb-6 flex justify-center gap-4 border-y-2 border-[#b9f6ca] py-3">
+	<a class:active={$page.url.pathname === '/record'} sveltekit:prefetch href="/record"
 		><p class="font-Josefin font-normal text-[#b9f6ca]">Record</p></a
 	>
-	<a
-		class:active={$page.url.pathname === '/bookshelf'}
-		sveltekit:prefetch
-		href="/bookshelf"
+	<a class:active={$page.url.pathname === '/bookshelf'} sveltekit:prefetch href="/bookshelf"
 		><p class="font-Josefin font-normal text-[#b9f6ca]">Bookshelf</p></a
 	>
-	<a
-		class:active={$page.url.pathname === '/invite'}
-		sveltekit:prefetch
-		href="/invite"
+	<a class:active={$page.url.pathname === '/invite'} sveltekit:prefetch href="/invite"
 		><p class="font-Josefin font-normal text-[#b9f6ca]">Invite</p></a
 	>
-	<a
-		class:active={$page.url.pathname === '/logout'}
-		sveltekit:prefetch
-		href="/logout"
+	<a class:active={$page.url.pathname === '/logout'} sveltekit:prefetch href="/logout"
 		><p class="font-Josefin font-normal text-[#b9f6ca]">Log out</p></a
 	>
 </nav>
 
 <style>
-
-
 </style>
