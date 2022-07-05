1e0d5a7cf3f7ea7f863901d52c6c8054522954aa
321jellyfish <csljeffreys@gmail.com>
2022-07-05 10:21:00 +0100

    style nav

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <header>
-	<Nav />
 	<a href="/">
-		<div class="mb-6 flex items-center p-4">
+		<div class="flex items-center p-4">
 			<img class="w-3/12" {src} alt="owly" />
 			<h1 class="font-Amatic text-6xl text-amber-100">Read Me A Story</h1>
 		</div>
 	</a>
 </header>
+<Nav />
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,17 +2,34 @@
 	import { page } from '$app/stores';
 </script>
 
-<a
-	class:active={$page.url.pathname === '/invite'}
-	sveltekit:prefetch
-	href="/invite"
-	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
-	><p class="text-right font-Josefin font-normal text-amber-100">Invite family members</p></a
->
-<a
-	class:active={$page.url.pathname === '/logout'}
-	sveltekit:prefetch
-	href="/logout"
-	class="underline decoration-amber-100 decoration-solid decoration-2 underline-offset-4"
-	><p class="text-right font-Josefin font-normal text-amber-100">Log out</p></a
->
+<nav class="flex gap-4 justify-center py-3 mx-3 mb-6 border-y-2 border-[#b9f6ca]">
+	<a
+		class:active={$page.url.pathname === '/record'}
+		sveltekit:prefetch
+		href="/record"
+		><p class="font-Josefin font-normal text-[#b9f6ca]">Record</p></a
+	>
+	<a
+		class:active={$page.url.pathname === '/bookshelf'}
+		sveltekit:prefetch
+		href="/bookshelf"
+		><p class="font-Josefin font-normal text-[#b9f6ca]">Bookshelf</p></a
+	>
+	<a
+		class:active={$page.url.pathname === '/invite'}
+		sveltekit:prefetch
+		href="/invite"
+		><p class="font-Josefin font-normal text-[#b9f6ca]">Invite</p></a
+	>
+	<a
+		class:active={$page.url.pathname === '/logout'}
+		sveltekit:prefetch
+		href="/logout"
+		><p class="font-Josefin font-normal text-[#b9f6ca]">Log out</p></a
+	>
+</nav>
+
+<style>
+
+
+</style>
