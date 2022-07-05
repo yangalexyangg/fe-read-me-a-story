cf176034ca7e808508f9667742f0eb5f3e61e45d
321jellyfish <csljeffreys@gmail.com>
2022-07-05 15:01:47 +0100

    add attribution

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,3 +42,10 @@
 		</li>
 	</ul>
 {/if}
+
+<footer class="text-[#b9f6ca] text-center italic mt-24 border-t-2 border-[#b9f6ca] mx-3 py-3">
+	<p class="not-italic">All icons created by Freepik - Flaticon:</p>
+	<a href="https://www.flaticon.com/free-icons/owl" target="blank" title="owl icons">Owl icons</a><br>
+	<a href="https://www.flaticon.com/free-icons/microphone" target="blank" title="microphone icons">Microphone icons</a><br>
+	<a href="https://www.flaticon.com/free-icons/book" target="blank" title="book icons">Book icons </a>
+</footer>
